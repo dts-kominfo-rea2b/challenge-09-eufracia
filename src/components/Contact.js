6322e9acff10aaef52042f5635ce30321ce4b776
eufracia <ziporalicia@gmail.com>
2022-06-19 20:53:02 +0700

    Adding contact and header styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,18 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './contact.css';
 
-const Contact = () => {
+const Contact = ({ data }) => {
     return (
-        <></>
+        <div class="row">
+            <img src={ data.photo } alt='profile-pic'/>
+            <div class="desc">
+                <p>{ data.name }</p>
+                <p>{ data.phone }</p>
+                <p>{ data.email }</p>
+            </div>
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './header.css';
 
 const Header = () => {
     return (
-        <></>
+        <header>
+            <h1>Call a Friend</h1>
+            <p>Your friendly contact app</p>
+            <hr />
+        </header>
     )
 }
 
